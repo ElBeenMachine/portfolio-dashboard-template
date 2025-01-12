99e935416c763db58d7c9484cb6b1a6b948220c1
“Ollie <ollie@beenhamow.co.uk>
2025-01-12 18:08:05 +0000

    Add Google authentication provider to NextAuth configuration

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -7,6 +7,8 @@
 
 import NextAuth from "next-auth";
 import EntraID from "next-auth/providers/microsoft-entra-id";
+import Google from "next-auth/providers/google";
+
 import { env } from "next-runtime-env";
 
 import Credentials from "next-auth/providers/credentials";
@@ -59,6 +61,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 			clientId: env("AUTH_MICROSOFT_ENTRA_ID_ID"),
 			clientSecret: env("AUTH_MICROSOFT_ENTRA_ID_SECRET"),
 			issuer: env("AUTH_MICROSOFT_ENTRA_ID_ISSUER"),
+		}),
+		Google({
+			clientId: env("AUTH_GOOGLE_ID"),
+			clientSecret: env("AUTH_GOOGLE_SECRET"),
 		}),
 	],
 	pages: {
