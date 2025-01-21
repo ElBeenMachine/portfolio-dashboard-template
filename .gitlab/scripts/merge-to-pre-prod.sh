2175759610253d169f817f19b1bc634c2e34b828
“Ollie <ollie@beenhamow.co.uk>
2025-01-21 19:52:14 +0000

    Update merge scripts to use GITLAB_PRIVATE_TOKEN for authorization

--- .gitlab/scripts/merge-to-pre-prod.sh
+++ .gitlab/scripts/merge-to-pre-prod.sh
@@ -8,7 +8,7 @@ request_body=$(jq -c -r -n --arg title "$(echo $CI_COMMIT_TAG | head -n 1)" '{ti
 ## Debug
 echo $request_body
 
-created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${PERSONAL_ACCESS_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=pre-production")
+created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=pre-production")
 
 ## Debug
 echo $created_merge_request_response
