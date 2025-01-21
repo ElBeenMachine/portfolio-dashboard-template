bf18ba2cd6922cc575ab2532dc46a6d0299caafb
“Ollie <ollie@beenhamow.co.uk>
2025-01-21 20:19:10 +0000

    Refactor CI stages for merge requests and update authorization method in merge scripts

--- .gitlab/scripts/merge-to-pre-prod.sh
+++ .gitlab/scripts/merge-to-pre-prod.sh
@@ -6,14 +6,12 @@ git fetch --no-tags
 request_body=$(jq -c -r -n --arg title "$(echo $CI_COMMIT_TAG | head -n 1)" '{title: $title}')
 
 ## Debug
-echo $request_body
+# echo $request_body
 
-req_url="https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=main&target_branch=pre-production&labels=release"
-
-created_merge_request_response=$(curl -X POST --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" $req_url)
+created_merge_request_response=$(curl -X POST --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=main&target_branch=pre-production&labels=release")
 
 ## Debug
-echo $created_merge_request_response
+# echo $created_merge_request_response
 
 merge_request_url=$(echo $created_merge_request_response | tr '\r\n' ' ' | jq '.web_url' | tr -d '"')
 
--- .gitlab/scripts/merge-to-prod.sh
+++ .gitlab/scripts/merge-to-prod.sh
@@ -6,12 +6,12 @@ git fetch --no-tags
 request_body=$(jq -c -r -n --arg title "$(echo $CI_COMMIT_TAG | head -n 1)" '{title: $title}')
 
 ## Debug
-echo $request_body
+# echo $request_body
 
-created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=production")
+created_merge_request_response=$(curl -X POST --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=pre-production&target_branch=production&labels=deployment")
 
 ## Debug
-echo $created_merge_request_response
+# echo $created_merge_request_response
 
 merge_request_url=$(echo $created_merge_request_response | tr '\r\n' ' ' | jq '.web_url' | tr -d '"')
 
