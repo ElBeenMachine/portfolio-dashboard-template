6d8176c95dcbca2b57ee4f12004a4e127a44c932
“Ollie <ollie@beenhamow.co.uk>
2025-01-21 20:20:24 +0000

    Update merge request titles and labels in deployment scripts for consistency

--- .gitlab/scripts/merge-to-prod.sh
+++ .gitlab/scripts/merge-to-prod.sh
@@ -3,12 +3,12 @@
 # Need all remote branches
 git fetch --no-tags
 
-request_body=$(jq -c -r -n --arg title "$(echo $CI_COMMIT_TAG | head -n 1)" '{title: $title}')
+request_body=$(jq -c -r -n --arg title "Deploy Version $(echo $CI_COMMIT_TAG | head -n 1)" '{title: $title}')
 
 ## Debug
 # echo $request_body
 
-created_merge_request_response=$(curl -X POST --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=pre-production&target_branch=production&labels=deployment")
+created_merge_request_response=$(curl -X POST --header "PRIVATE-TOKEN: ${GITLAB_PRIVATE_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=pre-production&target_branch=production&labels=Deployment")
 
 ## Debug
 # echo $created_merge_request_response
